e1650ee11b38f4ff9249cd768a837bbe21efecde
CaroBedoya <jennycaritobedoya@gmail.com>
2024-07-08 16:55:47 -0500

    sprint 5

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Tripleten web_project_around
+
+## Descripcion y funcionalidad
+
+El web project around es el proyecto final del Sprint 5 en el cual tuvimos un primer acercamiento con elementos básicos de JavaScript, así como su interacción con HTML y CSS.
+
+Este proyecto se basa en la creación de un sitio web similar a una red social con un perfil y con imagenes en este caso de Estados Unidos;en el se permite al usuario interactuar y crear su propio perfil mediante un formulario y JavaScript, además se pueden agregar distintas imágenes a las ya establecidas. Por ahora se genera una pagina donde se puede practicar lo aprendido hasta ahora.
+
+# Tecnologias y tecnicas utilizadas
+
+- HTML
+- CSS
+- Flexbox
+- Medias queries
+- Metodología BEM
+- Grid
+- GitHub
+- Formularios
+- Fonts
+- JavaScript
+- Figma
---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const popupProfile = document.querySelector("#popup-profile");
+const profileButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__username");
+const profileAbout = document.querySelector(".profile__paragraph");
+const inputName = document.querySelector("#input-name");
+const inputAbout = document.querySelector("#input-about");
+const formProfile = document.querySelector("#form-profile");
+
+function handleOpenProfile(evt) {
+  popupProfile.classList.add("popup__show");
+}
+
+function clearInputs() {
+  inputName.value = "";
+  inputAbout.value = "";
+}
+
+function handleCloseProfile() {
+  popupProfile.classList.remove("popup__show");
+}
+profileButton.addEventListener("click", handleOpenProfile);
+
+formProfile.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  handleCloseProfile();
+});
+
+function handleOpenProfile(evt) {
+  console.log("Abriendo perfil");
+  popupProfile.classList.add("popup__show");
+}
+
+function handleCloseProfile() {
+  console.log("Cerrando perfil");
+  popupProfile.classList.remove("popup__show");
+}
