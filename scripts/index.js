517d519db2e312fbd5eb0f6fe41d76e224de14b9
CaroBedoya <jennycaritobedoya@gmail.com>
2024-07-08 21:13:09 -0500

    correccion sprint5

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const profileAbout = document.querySelector(".profile__paragraph");
 const inputName = document.querySelector("#input-name");
 const inputAbout = document.querySelector("#input-about");
 const formProfile = document.querySelector("#form-profile");
+const closeButton = document.querySelector(".form__close-button");
 
 function handleOpenProfile(evt) {
   popupProfile.classList.add("popup__show");
@@ -18,12 +19,17 @@ function clearInputs() {
 function handleCloseProfile() {
   popupProfile.classList.remove("popup__show");
 }
+
 profileButton.addEventListener("click", handleOpenProfile);
 
 formProfile.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
+  handleCloseProfile();
+});
+
+closeButton.addEventListener("click", function () {
   handleCloseProfile();
 });
 
