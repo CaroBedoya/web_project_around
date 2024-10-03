f1bb641614439c8f1d46498f88321a13b6fb1020
CaroBedoya <jennycaritobedoya@gmail.com>
2024-10-02 20:02:17 -0500

    version corregida project 8

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,103 @@
 const popupProfile = document.querySelector("#popup-profile");
+const popupCards = document.querySelector("#popup-add-card");
 const profileButton = document.querySelector(".profile__edit-button");
+const openCardForm = document.querySelector(".profile__add-container");
 const profileName = document.querySelector(".profile__username");
 const profileAbout = document.querySelector(".profile__paragraph");
 const inputName = document.querySelector("#input-name");
 const inputAbout = document.querySelector("#input-about");
 const formProfile = document.querySelector("#form-profile");
-const closeButton = document.querySelector(".form__close-button");
+const formCards = document.querySelector("#form-addcard");
+const closeButtons = document.querySelectorAll(".form__close-button");
+const template = document.querySelector(".template-card");
+const cardArea = document.querySelector(".elements");
+const inputTitle = document.querySelector("#input-title");
+const inputLink = document.querySelector("#input-url");
 
-function handleOpenProfile(evt) {
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+function cardGenerator(name, link) {
+  const card = template.content.cloneNode(true).querySelector(".element");
+  const cardImage = card.querySelector(".element__photo");
+  const cardTitle = card.querySelector(".element__photo-name");
+  const btnDelete = card.querySelector(".element__photo-delete");
+  const btnLike = card.querySelector(".element__photo-heart");
+
+  btnLike.addEventListener("click", function () {
+    btnLike.classList.toggle("element__photo-heart_active");
+  });
+
+  btnDelete.addEventListener("click", function () {
+    card.remove();
+  });
+
+  cardImage.src = link;
+  cardTitle.textContent = name;
+  cardImage.alt = name;
+  return card;
+}
+
+initialCards.forEach(function (element) {
+  const newCard = cardGenerator(element.name, element.link);
+  cardArea.append(newCard);
+});
+
+function handleOpenProfile() {
   popupProfile.classList.add("popup__show");
 }
 
-function clearInputs() {
-  inputName.value = "";
-  inputAbout.value = "";
+function handleClosePopup(popup) {
+  popup.classList.remove("popup__show");
 }
 
-function handleCloseProfile() {
-  popupProfile.classList.remove("popup__show");
-}
+closeButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    handleClosePopup(button.closest(".popup"));
+  });
+});
 
 profileButton.addEventListener("click", handleOpenProfile);
+
+openCardForm.addEventListener("click", function () {
+  popupCards.classList.add("popup__show");
+});
 
 formProfile.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  handleCloseProfile();
+  handleClosePopup(popupProfile);
 });
 
-closeButton.addEventListener("click", function () {
-  handleCloseProfile();
+formCards.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const cardToAdd = cardGenerator(inputTitle.value, inputLink.value);
+  cardArea.prepend(cardToAdd);
+  handleClosePopup(popupCards);
+  formCards.reset();
 });
-
-function handleOpenProfile(evt) {
-  console.log("Abriendo perfil");
-  popupProfile.classList.add("popup__show");
-}
-
-function handleCloseProfile() {
-  console.log("Cerrando perfil");
-  popupProfile.classList.remove("popup__show");
-}
